8772481386b10f64debca3f18aad6ac2c7da19fc
Alex Karalanian <alex.karalanian@gmail.com>
2024-08-19 23:45:23 -0400

    added individual meal page content

--- app/meals/[id]/page.js
+++ app/meals/[id]/page.js
@@ -1,3 +1,32 @@
-export default function Page() {
-  return <h1>Meal ID</h1>;
+import { getMeal } from '@/lib/meals';
+import styles from './page.module.css';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+export default function Page({ params }) {
+  const meal = getMeal(params.id);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} fill alt="image" />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={styles.instructions}>{meal.instructions}</p>
+      </main>
+    </>
+  );
 }
---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="error">
+      <h1>Meal Not Found</h1>
+      <p>Failed to find meal data...</p>
+    </main>
+  );
+}
