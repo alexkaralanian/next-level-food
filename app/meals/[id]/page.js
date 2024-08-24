fd4b79a7f02c6167759f61140ba250675425aa4a
Alex Karalanian <alex.karalanian@gmail.com>
2024-08-24 13:37:08 -0400

    added static and dynamic metadata

--- app/meals/[id]/page.js
+++ app/meals/[id]/page.js
@@ -3,12 +3,21 @@ import styles from './page.module.css';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
-export default function Page({ params }) {
+export async function generateMetadata({ params }) {
   const meal = getMeal(params.id);
 
   if (!meal) {
     notFound();
   }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.id);
 
   return (
     <>
